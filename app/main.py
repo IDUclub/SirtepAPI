6e662b0a3418e20917a2798f65887d1ad2f670e6
Leon <turkov.leon-2@yandex.ru>
2025-10-20 15:33:17 +0300

    refactor(main): - updated app version

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
     lifespan=lifespan,
     title="Sertep API",
     description="API for scheduling project construction",
-    version="0.1.2",
+    version="0.2.0",
 )
 
 origins = ["*"]
